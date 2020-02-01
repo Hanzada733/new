4339f9afd9cb8c2ce81a61cab0a1fa72c100e6b9
Hanzada733 <hanzada733@gmail.com>
2020-02-01 18:03:34 +0600

    first commit

---
+++ mysite/devsettings.py
@@ -0,0 +1,15 @@
+from .settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+DEBUG = True
+ROOT_URLCONF = 'mysite.devurls'
+
